82a850896713667fd426b957001d74b7de5d8aa2
81673211 <81673211@qq.com>
2019-04-29 13:42:22 +0800

    init config

---
+++ src/main/java/com/fred/ycwl/config/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.fred.ycwl.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ *
+ * <b>Description:.</b><br> 
+ * @author <b>sil.zhou</b>
+ * <br><b>ClassName:</b> 
+ * <br><b>Date:</b> 2019/4/12 16:13
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
